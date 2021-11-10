3fe092e9f56f26f71f7510cb04bf16576cce1c05
shamiOuO <93506900+shamiOuO@users.noreply.github.com>
2021-11-10 12:16:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import time
 from weather_message import WeatherStation
 from Line_notify import send_message
 import logging
-Day = 60*60*24
+Day = 60*60*24 #設定多久傳一次
 userdatas = [{
-    'owm_api_kei': '3d3bf97fa5eec6700f1b8f5366bb200b',
-    'Line_Token': 'YfNoheEHU9pQZzV5zd0uvFaG3XnkoTa4sxyxBn1fcHO',
-    'city': 'Yulin',
-    'country': 'Taiwan'
+    'owm_api_kei': 'your_api_kei',
+    'Line_Token': 'your_line_token',
+    'city': 'your_city',
+    'country': 'your_country'
 }]
 
 
@@ -15,7 +15,7 @@ def main():
     while True:
         for userdata in userdatas:
             weatherstation = WeatherStation(userdata.get('owm_api_kei'))
-            weatherdata = weatherstation.get_weather_at_place(userdata.get('city', 'Yulin'),
+            weatherdata = weatherstation.get_weather_at_place(userdata.get('city', 'Taipei'),
                                                               userdata.get('country', 'Taiwan')),
             status_code = send_message(userdata.get('Line_Token'),weatherdata)
             print(status_code)
